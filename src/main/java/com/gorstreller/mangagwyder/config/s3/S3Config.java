e2adb4462c8f3265a5e1f40f81c84b7ce73978a0
Kirill Vlasovets <k.vlasovets@a1qa.com>
2024-09-14 03:58:10 +0700

    ApplicationRunner bean moved to config class

--- src/main/java/com/gorstreller/mangagwyder/config/s3/S3Config.java
+++ src/main/java/com/gorstreller/mangagwyder/config/s3/S3Config.java
@@ -5,11 +5,16 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.gorstreller.mangagwyder.service.s3.S3Service;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Log4j2
 public class S3Config {
 
     @Value("${aws.access.key}")
@@ -28,5 +33,19 @@ public class S3Config {
                 .withRegion(Regions.EU_CENTRAL_1)
                 .enableAccelerateMode()
                 .build();
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner(S3Service s3Service) {
+        return args -> {
+            log.info("Spring Boot AWS S3 integration...");
+
+            try {
+                var s3Object = s3Service.getFile("jvm.jpg");
+                log.info(s3Object.getKey());
+            } catch (AmazonS3Exception e) {
+                log.error(e.getMessage());
+            }
+        };
     }
 }
